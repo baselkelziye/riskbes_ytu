d4b3bc1edbb7495a668a64c966ec6aa2165c15ef
Basel KELZİYE (Pavelsis) <basel.kelziye@pavelsis.com>
2024-05-20 13:53:38 +0300

    EX asamasi instruction_execution_stage modulu altinda birlestirildi

--- hdl/core.v
+++ hdl/core.v
@@ -89,10 +89,8 @@ module core(
     wire PC_sel_w_ex_mem_o;
     
 // forwarding unit                   
-    wire [31:0] alu_in1_forwarded_input;
-    wire [31:0] alu_in2_forwarded_input;
-    wire [1:0]  forwardA;
-    wire [1:0]  forwardB;
+
+
     
     wire [2:0] funct3_ex_mem_o;
     wire [6:0] funct7_ex_mem_o;
@@ -122,8 +120,7 @@ module core(
     //tmp control signals
     wire id_stall;
     wire if_stall = id_stall | ~data_cache_blocking_n_last | ~data_cache_blocking_n_i;
-    wire [31:0] alu_in1_w;
-    wire [31:0] alu_in2_w; 
+
     wire [31:0] reg_wb_data_w;
     wire ins_busy_w;
     wire busy_w;
@@ -192,214 +189,79 @@ module core(
    
 
 
-//sonrasi icin EX asamasinin olusturulmasi
+
+
+    
+    //sonrasi icin EX asamasinin olusturulmasi
    
-//   instruction_execution_stage u_ex(
-//       .clk_i(clk_i),
-//       .rst_i(rst_i)
-//   );
-            
-    //Dallanma birimi (umutuun raporuna bakilmali)
-    branch_jump u_branch_jump(
-        .in1_i(alu_in1_w),          //alu output yap
-        .in2_i(alu_in2_w),
-        .is_branch_instr(is_branch_instr_id_ex_o),
-        .is_jump_instr(is_jump_instr_id_ex_o),
-        .funct3_i(funct3_id_ex_o),
-        .PC_sel_o(PC_sel_w)            //sinyal
-    );
-
-
-                 
-     //Yonlendirma birimi, kendi dosyasinda aciklama yapildi
-    forwarding_unit forwarding_unit(
-        .rd_label_ex_mem_o(rd_ex_mem_o),
-        .rd_label_mem_wb_o(rd_mem_wb_o),
-        .rs1_label_id_ex_o(rs1_label_id_ex_o),
-        .rs2_label_id_ex_o(rs2_label_id_ex_o),
-        .reg_wb_en_ex_mem_o(reg_wb_en_ex_mem_o),
-        .reg_wb_en_mem_wb_o(reg_wb_en_mem_wb_o),
-        .is_memory_instruction_mem_wb_o(is_memory_instruction_mem_wb_o),
-        .forwardA(forwardA),
-        .forwardB(forwardB)
-    );
+   instruction_execution_stage u_ex(
+       .clk_i(clk_i),
+       .rst_i(rst_i),
+       .busywait(busy_w),
+       .alu_out_ex_mem_o(alu_out_ex_mem_o),//internal
+       
+       .reg_wb_en_ex_mem_i(reg_wb_en_id_ex_o),
+       .reg_wb_en_ex_mem_o(reg_wb_en_ex_mem_o),
+       
+       .rd_ex_mem_i(rd_id_ex_o), //in_id nin cikisini ver buraya
+       .rd_ex_mem_o(rd_ex_mem_o),
+       
+       
+       .imm_ex_mem_i(imm_value_id_ex_o),
+       .imm_ex_mem_o(imm_ex_mem_o),
+       
+       .pc_ex_mem_i(pc_id_ex_o),
+       .pc_ex_mem_o(pc_ex_mem_o),
+       
+       .wb_sel_ex_mem_i(wb_sel_id_ex_o),
+       .wb_sel_ex_mem_o(wb_sel_ex_mem_o),
+       
+       .rs1_label_ex_mem_i(rs1_label_id_ex_o), 
+       .rs1_label_ex_mem_o(rs1_label_ex_mem_o),
+       
+       .rs2_label_ex_mem_i(rs2_label_id_ex_o),
+       .rs2_label_ex_mem_o(rs2_label_ex_mem_o),
+       
+       .read_write_sel_ex_mem_i(read_write_sel_id_ex_o),
+       .read_write_sel_ex_mem_o(read_write_sel_ex_mem_o),
+       
+       .rs2_ex_mem_o(rs2_ex_mem_o),
+       
+       .is_memory_instruction_ex_mem_i(is_memory_instruction_id_ex_o),
+       .is_memory_instruction_ex_mem_o(is_memory_instruction_ex_mem_o),
+       
+       .PC_sel_w_ex_mem_o(PC_sel_w_ex_mem_o),
     
+       .funct3_ex_mem_i(funct3_id_ex_o),
+       .funct3_ex_mem_o(funct3_ex_mem_o),
+       
+       .funct7_ex_mem_i(funct7_id_ex_o),
+       .funct7_ex_mem_o(funct7_ex_mem_o),
+       
+       .is_load_instr_ex_mem_i(is_load_instr_id_ex_o),
+       .is_load_instr_ex_mem_o(is_load_instr_ex_mem_o),
+       
+       .is_store_instr_ex_mem_i(is_store_instr_id_ex_o), 
+       .is_store_instr_ex_mem_o(is_store_instr_ex_mem_o),
+       
+       //inputs for the EX stage from prev
+       .is_branch_instr_i(is_branch_instr_id_ex_o),
+       .is_jump_instr_i(is_jump_instr_id_ex_o),
+       
+       .rd_mem_wb_o(rd_mem_wb_o),
+       .reg_wb_en_mem_wb_o(reg_wb_en_mem_wb_o),
+       .is_memory_instruction_mem_wb_o(is_memory_instruction_mem_wb_o),
+       .rs1_value_ex_mem_i(rs1_value_id_ex_o),
+       .rs2_value_ex_mem_i(rs2_value_id_ex_o),
+       .alu_out_mem_wb_o(alu_out_mem_wb_o),
+       .rd_data_mem_wb_o(rd_data_mem_wb_o),
+       .alu_op1_sel_ex_mem_i(alu_op1_sel_id_ex_o),
+       .alu_op2_sel_ex_mem_i(alu_op2_sel_id_ex_o),
+       .funct5_ex_mem_i(funct5_id_ex_o),
+       .EX_op_ex_mem_i(EX_op_id_ex_o)
+     );
     
 
-    mux_4x1 #(
-    .DATA_WIDTH(32)
-    ) rs1_latest_value_selector (
-    .in0(rs1_value_id_ex_o),    // X0
-    .in1(alu_out_mem_wb_o),     // X1
-    .in2(alu_out_ex_mem_o),     // X2
-    .in3(rd_data_mem_wb_o),     // X3
-    .select(forwardA),
-    .out(alu_in1_w)
-    );
-
-    wire [31:0] rs1_tmp;
-    wire [31:0] shifted_rs1;
-        //EX_Decoder sinyalleri, Sonra yukariya EX Stage sinyallerin altina Aynen tasinsin
-    wire [1:0] chip_select;
-    wire [3:0] ALU_op;
-    wire [4:0] BMU_op;
-    wire [2:0] MDU_op;
-    wire [31:0] ALU_res, MDU_res, BMU_res;
-    wire rs1_shift_sel, rs2_negate_sel;
-    
-    mux_2x1 #(
-    .DATA_WIDTH(32)
-    ) pc_or_rs1_selector (
-    .in0(alu_in1_w),             // X0: Most recent rs1 value
-    .in1(pc_id_ex_o),            // X1: PC value
-    .select(alu_op1_sel_id_ex_o),
-    .out(rs1_tmp)
-    );
-    
-    barrel_shifter_32bit u_barrel_shifter (
-        .data_i(rs1_tmp),
-        .ctrl_i(funct3_id_ex_o[2:1]),// upper 2 bits of funct3 tells us the amount of shift
-        .data_o(shifted_rs1)
-    );
-    
-    //mux for Shifting RS1
-    mux_2x1 #(
-        .DATA_WIDTH(32)
-    ) u_rs1_shift_sel_mux(
-        .in0(rs1_tmp),
-        .in1(shifted_rs1),
-        .select(rs1_shift_sel), 
-        .out(alu_in1_forwarded_input)
-    );
-
-      
-    mux_4x1 #(
-    .DATA_WIDTH(32)
-    ) rs2_latest_value_selector (
-    .in0(rs2_value_id_ex_o),    // X0
-    .in1(alu_out_mem_wb_o),     // X1
-    .in2(alu_out_ex_mem_o),     // X2
-    .in3(rd_data_mem_wb_o),     // X3
-    .select(forwardB),
-    .out(alu_in2_w)
-    );
-
-    wire [31:0] rs2_tmp;
-    mux_2x1 #(
-    .DATA_WIDTH(32)
-    ) imm_or_rs2_selector (
-    .in0(alu_in2_w),                 // X0: Most recent rs2 value
-    .in1(imm_value_id_ex_o),         // X1: Immediate value
-    .select(alu_op2_sel_id_ex_o),
-    .out(rs2_tmp)
-    );
-
-    mux_2x1 #(
-        .DATA_WIDTH(32)
-    ) u_rs2_negate_sel_mux(
-        .in0(rs2_tmp),
-        .in1(~rs2_tmp),
-        .select(rs2_negate_sel),
-        .out(alu_in2_forwarded_input)
-    );
-
-    
-    EX_Decoder u_EX_Decoder(
-                   .EX_op (EX_op_id_ex_o), 
-                   .funct3(funct3_id_ex_o),
-                   .funct5(funct5_id_ex_o),
-                   .funct7(funct7_id_ex_o),
-                   .ALU_op(ALU_op),
-                   .BMU_op(BMU_op),
-                   .MDU_op(MDU_op),
-                   .chip_select(chip_select),
-                   .rs1_shift_sel(rs1_shift_sel),
-                   .rs2_negate_sel(rs2_negate_sel)
-    );
-    
-    
-    alu u_alu(
-        .alu1_i(alu_in1_forwarded_input),  //bunlar anlik cikis oldugu icin pipeline'a girmelerine gerek yok.
-        .alu2_i(alu_in2_forwarded_input),
-        .chip_select(chip_select),
-        .alu_op_i(ALU_op),
-        .result_o(ALU_res)
-    );
-
-    MDU u_MDU(
-        .alu1_i(alu_in1_forwarded_input),
-        .alu2_i(alu_in2_forwarded_input),
-        .chip_select(chip_select),
-        .MDU_op(MDU_op),
-        .result_o(MDU_res)
-    );
-    
-   
-                                   
-
-    
-    BMU u_BMU(
-        .rs1_value_i(alu_in1_forwarded_input),
-        .rs2_value_i(alu_in2_forwarded_input),
-        .BMU_opcode_i(BMU_op),
-        .BMU_result_o(BMU_res)
-    );
-
-
-    mux_4x1 #(
-    .DATA_WIDTH(32)
-    ) u_EX_res_select_mux (
-       .in0(ALU_res),        // X0
-       .in1(MDU_res),        // X1
-       .in2(BMU_res),   // X2
-       .in3(32'hdeadbeef),   // X3
-       .select(chip_select),
-       .out(alu_out_ex_mem_i)
-    );
-    
-    
-    ex_mem_stage_reg ex_mem(
-        .clk_i(clk_i),
-        .rst_i(rst_i),
-        
-        .busywait(busy_w),
-        .alu_out_ex_mem_i(alu_out_ex_mem_i),
-        .alu_out_ex_mem_o(alu_out_ex_mem_o),
-        .reg_wb_en_ex_mem_i(reg_wb_en_id_ex_o),
-        .reg_wb_en_ex_mem_o(reg_wb_en_ex_mem_o),
-        .rd_ex_mem_i(rd_id_ex_o), //in_id nin cikisini ver buraya
-        .rd_ex_mem_o(rd_ex_mem_o),
-        .pc_ex_mem_i(pc_id_ex_o),
-        .pc_ex_mem_o(pc_ex_mem_o),
-        .wb_sel_ex_mem_i(wb_sel_id_ex_o),
-        .wb_sel_ex_mem_o(wb_sel_ex_mem_o),
-        .imm_ex_mem_i(imm_value_id_ex_o),
-        .imm_ex_mem_o(imm_ex_mem_o),
-        .rs1_label_ex_mem_i(rs1_label_id_ex_o),
-        .rs1_label_ex_mem_o(rs1_label_ex_mem_o),
-        .rs2_label_ex_mem_i(rs2_label_id_ex_o),
-        .rs2_label_ex_mem_o(rs2_label_ex_mem_o),
-        .read_write_sel_ex_mem_i(read_write_sel_id_ex_o),
-        .read_write_sel_ex_mem_o(read_write_sel_ex_mem_o),
-        .rs2_ex_mem_i(alu_in2_w),
-        .rs2_ex_mem_o(rs2_ex_mem_o),
-        .is_memory_instruction_ex_mem_i(is_memory_instruction_id_ex_o),
-        .is_memory_instruction_ex_mem_o(is_memory_instruction_ex_mem_o),
-        .PC_sel_w_ex_mem_i(PC_sel_w),
-        .PC_sel_w_ex_mem_o(PC_sel_w_ex_mem_o),
-        .funct3_ex_mem_i(funct3_id_ex_o),
-        .funct3_ex_mem_o(funct3_ex_mem_o),
-        
-        .funct7_ex_mem_i(funct7_id_ex_o),
-        .funct7_ex_mem_o(funct7_ex_mem_o),
-
-        .is_load_instr_ex_mem_i(is_load_instr_id_ex_o),
-        .is_load_instr_ex_mem_o(is_load_instr_ex_mem_o),
-
-        .is_store_instr_ex_mem_i(is_store_instr_id_ex_o),
-        .is_store_instr_ex_mem_o(is_store_instr_ex_mem_o)
-    );
-    
    memory_stage u_mem(
       .clk_i(clk_i),
       .rst_i(rst_i),
