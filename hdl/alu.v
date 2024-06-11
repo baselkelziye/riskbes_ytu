6b6b675806d4620d1f2009fb7608dfec8475bd57
Ahmet Zahit CAN <ahmetzahitcan@gmail.com>
2024-06-11 21:19:22 +0300

    main'i düzeltmeye çalışıyorum

--- hdl/alu.v
+++ hdl/alu.v
@@ -1,10 +1,98 @@
+// `timescale 1ns / 1ps
+// module alu (
+//    input [31:0]alu1_i,
+//    input [31:0]alu2_i,
+//    input [4:0] alu_op_i,
+//    output [31:0]result_o
+// );
+
+//    reg [63:0]mul_r;
+//    reg [31:0]result_r;
+   
+//    assign result_o = result_r;
+
+
+// //****** SUM Icin Sinyaller ve Module Tanimi
+//    wire [31:0] sum_result;
+//    wire sum_cout;
+
+//    cla32 u_cla32(
+//       .d1(alu1_i),
+//       .d2(alu2_i),
+//       .cin(1'b0),
+//       .sum(sum_result),
+//       .cout(sum_cout)
+//    );
+//    // Eklenen Module Sonucu sensitivity CASE e eklenmeli, Ilerde "*" yapilsin         
+//    always @(alu1_i or alu2_i or alu_op_i or sum_result) begin
+        
+//       case (alu_op_i)
+//          5'b00000: result_r = sum_result; //ADD
+//          5'b00010: result_r = alu1_i - alu2_i; //SUB     
+//          5'b00100: result_r = alu1_i << alu2_i; //SLL      
+//          5'b01000: result_r = $signed(alu1_i) < $signed(alu2_i) ? 1 : 0; //SLT
+//          5'b01100: result_r = alu1_i < alu2_i ? 1 : 0; //SLTU
+//          5'b10000: result_r = alu1_i ^ alu2_i; //XOR
+//          5'b10100: result_r = alu1_i >> alu2_i; //SRL
+//          5'b10110: result_r = $signed(alu1_i) >>> alu2_i; //SRA    
+//          5'b11000: result_r = alu1_i | alu2_i; //OR
+//          5'b11100: result_r = alu1_i & alu2_i; //AND 
+//          5'b00001: result_r = alu1_i * alu2_i; //MUL
+//          5'b00101: begin  //MULH 
+//             mul_r = $signed(alu1_i) * $signed(alu2_i);
+//             result_r = mul_r[63:32];
+//          end
+//          5'b01001: begin //MULHU
+//             mul_r = $unsigned(alu1_i) * $unsigned(alu2_i);
+//             result_r = mul_r[63:32];
+//          end
+//          5'b01101: begin //MULHSU    
+//             mul_r = $signed(alu1_i) * $unsigned(alu2_i);
+//             result_r = mul_r[63:32];
+//          end
+//          5'b10001: begin //DIV
+//             if(alu2_i == 32'h0) begin
+//                result_r = -32'h1; 
+//             end else if(alu1_i[31] == 1'b1 && alu2_i == -32'h1) begin 
+//                result_r = alu1_i;
+//             end else begin 
+//                result_r = $signed(alu1_i) / $signed(alu2_i);
+//             end
+//          end
+//          5'b10101: begin //DIVU
+//             if(alu2_i == 32'h0) begin
+//                result_r = (2**32)-1;
+//             end else begin 
+//                result_r = $unsigned(alu1_i) / $unsigned(alu2_i);
+//             end
+//          end
+//          5'b11001: begin //REM       
+//             if(alu2_i == 32'h0) begin
+//                result_r = alu1_i; 
+//             end else if(alu1_i[31] == 1'b1 && alu2_i == -32'h1) begin 
+//                result_r = 32'h0;
+//             end else begin 
+//                result_r = $signed(alu1_i) % $signed(alu2_i);
+//             end
+//          end
+//          5'b11101: begin //REMU       
+//             if(alu2_i == 32'h0) begin
+//                result_r = alu1_i;
+//             end else begin 
+//                result_r = $unsigned(alu1_i) % $unsigned(alu2_i);
+//             end
+//          end
+//       endcase
+//    end         
+
+// endmodule
 module alu (input [31:0]alu1_i,
             input [31:0]alu2_i,
-            input [3:0] alu_op_i,
+            input [4:0] alu_op_i,
             input [1:0] chip_select,
             output [31:0]result_o);
 
-            
+            reg [63:0]mul_r;
             reg [31:0]result_r;
             
             
@@ -13,52 +101,35 @@ module alu (input [31:0]alu1_i,
           cla32 u_cla32(
              .d1(alu1_i),
              .d2(alu2_i),
-             .cin(alu_op_i[0]),
+             .cin(1'b0),
              .sum(sum_result),
              .cout(sum_cOut)
           );
-        /*
-        0000 ADD
-        0001 SUB
-        0011 SLL
-        0100 SLT
-        0101 SLTU
-        0110 XOR
-        0111 SRL
-        1000 SRA
-        1001 OR
-        1010 AND
-        1011 XNOR
-        */
-
-          wire [31:0] xor_result;
-          assign xor_result = alu1_i ^ alu2_i;
+   
             
-            always @ (*) begin
+            always @ (alu1_i or alu2_i or alu_op_i or sum_result) begin
               if(chip_select == 2'b00) begin
                 case (alu_op_i)
-                4'b0000:   //ADD 
+                5'b00000:   //ADD 
                 result_r = sum_result;
-                4'b0001:   //SUB
-                result_r = sum_result;
-                4'b0011:   //SLL
+                5'b00010:   //SUB
+                result_r = alu1_i - alu2_i;
+                5'b00100:   //SLL
                 result_r = alu1_i << alu2_i;
-                4'b0100:   //SLT
+                5'b01000:   //SLT
                 result_r = alu1_i < $signed(alu2_i) ? 1 : 0;
-                4'b0101:   //SLTU
+                5'b01100:   //SLTU
                 result_r = alu1_i < alu2_i ? 1 : 0;
-                4'b0110:   //XOR
-                result_r = xor_result;
-                4'b0111:   //SRL
+                5'b10000:   //XOR
+                result_r = alu1_i ^ alu2_i;
+                5'b10100:   //SRL
                 result_r = alu1_i >> alu2_i;
-                4'b1000:   //SRA
+                5'b10110:   //SRA
                 result_r = $signed(alu1_i) >>> alu2_i;
-                4'b1001:   //OR
+                5'b11000:   //OR
                 result_r = alu1_i | alu2_i;
-                4'b1010:   //AND
+                5'b11100:   //AND
                 result_r = alu1_i & alu2_i;
-                4'b1011:  //XNOR
-                result_r = ~xor_result;
                 endcase
               end else begin
                 result_r = result_o;
--- tests/program_tests/bmu_test.s
+++ tests/program_tests/bmu_test.s
@@ -1,39 +1,33 @@
 xor x5,x1,x1 
 xor x6, x1,x1
 xor x7,x1,x1
-lui x5,0xdeadc      #x5 = 0xdeadc000
-xori x6, x0, 0xeef  #x6 = 0x00000eef
-add x7, x5,x6       #x7 = 0xdeadbeef
+lui x5,0xdeadc #x5 = 0xdeadc000
+xori x6, x0, 0xeef #x6 = 0x00000eef
+add x7, x5,x6 #x7 = 0xdeadbeef
 addi x0,x0,0
-lui x1, 0x000AA     #x1 = 0x000AA000
-addi x2, x0,2       #x2 = 0x00000002
-addi x3,x0, 0x64    #x3 = 0x00000064
-addi x4,x0, 0x1F    #x4 = 0x0000001F
-lui x5, 0xFFFFF     #x5 = 0xFFFFF000
-addi x0,x0,0        #
-addi x0,x0,0        # do differentiate the machine codes
-clz x31, x1         # 60009f93  x31 = 0x0000000c
-ctz x30, x1         # 60109f13  x30 = 0x0000000d
-cpop x29, x1        # 60209e93  x29 = 0x00000004 
-orc.b x28, x1       # 2870de13  x28 = 0x00ffff00
-rev8 x27, x7        # 6983dd93  x27 = 0xefbeadde
-zext.h x26, x7      # 0803cd33  x26 = 0x0000beef 
-sext.h x25, x1      # 60509c93  x25 = 0xffffbeef
-sext.b x24, x7      # 60439c13  x24 = ffffffffef
-rol x23, x3, x2     # 60219bb3 x23 = 0x190
-ror x22, x3, x2     # 6021db33 x22 = 0x19
-rori x21, x3, 2     # 6021da93 x21 = 0x19
-bext x18, x5, x4    # 4842d933 
-bexti x17, x5, 31   # 49f2d893 
-bclr x20, x5, x4    # 48429a33
-bclri x19, x5, 31   # 49f29993 
-binv x16, x5, x4    # 68429833
-binvi x15, x5, 31   # 69f29793
-bset x14, x5, x4    # 28429733
-bseti x13, x5, 31   # 29f29693 
-sh1add x12, x3, x2  # 20312633
-sh2add x11, x3, x2  # 203145b3
-sh3add x10, x3, x2  # 20316533
-andn x9, x7, x14    # 40e3f4b3
-orn x8, x7, x14     # 40e3e433
-xnor x1, x16, x15   # 40f840b3
+lui x1, 0x000AA #x1 = 0x000AA000
+addi x2, x0,2 #x2 = 0x00000002
+addi x3,x0, 0x64 #x3 = 0x00000064
+addi x4,x0, 0x1F #x4 = 0x0000001F
+lui x5, 0xFFFFF #x5 = 0xFFFFF000
+addi x0,x0,0
+addi x0,x0,0 # do differentiate the machine codes
+clz x31, x1
+ctz x30, x1
+cpop x29, x1
+orc.b x28, x1
+rev8 x27, x7
+zext.h x26, x7
+sext.h x25, x1
+sext.b x24, x7
+rol x23, x3, x2
+ror x22, x3, x2
+rori x21, x3, 2
+bext x18, x5, x4
+bexti x17, x5, 31
+bclr x20, x5, x4
+bclri x19, x5, 31
+binv x16, x5, x4
+binvi x15, x5, 31
+bset x14, x5, x4
+bseti x13, x5, 31
