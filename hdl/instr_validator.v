cb2207003980a8adbbeb0136fa9821f2cd0e6548
Ahmet Zahit CAN <ahmetzahitcan@gmail.com>
2024-07-02 15:50:04 +0300

    Psödo-buyrukların yazılım tarafına gözükmesi engellendi.

--- hdl/instr_validator.v
+++ hdl/instr_validator.v
@@ -3,11 +3,12 @@ module instr_validator(
    output [31:2] instr_o
 );
 
-   localparam INSTR_ILLEGAL = {30{1'b1}};
+   localparam INSTR_ILLEGAL = {{25{1'bX}}, {5{1'b1}}};
 
    wire [31:2] instr = data_i[31:2];
    wire [1:0] quadrant = data_i[1:0];
+   wire [2:0] opcode_mid = data_i[4:2];
 
-   assign instr_o = (quadrant == 2'b11) ? instr : INSTR_ILLEGAL;
+   assign instr_o = (opcode_mid != 3'b111 && quadrant == 2'b11) ? instr : INSTR_ILLEGAL;
 
 endmodule
